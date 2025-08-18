4f1c06843196cc2c0b995d1f3df8944ab10f0a6d
SamKurb <zcabsku@ucl.ac.uk>
2025-08-18 20:14:40 +0100

    completed ex06, ex07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(str) 
+{
+    let charArray = str.split("");
+    let left = 0
+    let right = charArray.length - 1
 
+    while (left < right)
+    {
+        let temp = charArray[left]
+
+        charArray[left] = charArray[right];
+        charArray[right] = temp;
+
+        ++left;
+        --right;
+    }
+
+    return charArray.join("");
 };
 
 // Do not edit below this line
